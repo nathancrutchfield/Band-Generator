78d2e07d3c2f134bb76407043f10f8febba7afdf
nathanCrutch42 <32715312-nathanCrutch42@users.noreply.replit.com>
2024-03-11 00:21:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Write your code below this line 👇
+print("Welcome to Band Name Generator")
+print("What is the name of the City you grew up in?")
+city = input()
+print("What is the name of your pet?")
+pet = input()
+print("The name of your band is " + city + " " + pet+"")
